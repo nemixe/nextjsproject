62b01a804165fd588690e03b800800cb9db25340
nemixe <rafel.ami@gmail.com>
2018-09-04 11:41:19 +0700

    less

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,22 +5,29 @@ const { SubMenu, ItemGroup } = Menu
 
 export default () => {
   return (
-    <Menu mode="horizontal">
-      <Menu.Item>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link href="/tes/tes">
-          <a>tes</a>
-        </Link>
-      </Menu.Item>
-      <Menu.Item>
-        <Link href="/tes/redux">
-          <a>redux</a>
-        </Link>
-      </Menu.Item>
-    </Menu>
+    <div>
+      <Menu mode="horizontal" style={{background: '#02A1F7'}}>
+        <Menu.Item className="tes">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/tes/tes">
+            <a>tes</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/tes/redux">
+            <a>redux</a>
+          </Link>
+        </Menu.Item>
+      </Menu>
+      <style jsx>{`
+        #tes {
+          color: red;
+        }
+      `}</style>
+    </div>
   )
 }
